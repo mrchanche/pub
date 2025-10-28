bb506f8ba6848ec8f8e123eb8daff833746b2535
mrchanche <68042455+mrchanche@users.noreply.github.com>
2025-10-28 16:08:28 -0500

    added audio download script

---
+++ system-setup/thematics/music.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# yt-dlp should already be installed but if now
+#pip install yt-dlp
+
+# -P "download/folder/here"
+# -x download audio only
+# --audio-format mp3 ... the audio format
+
+# Mostly electronic background music
+
+#yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=IdoWuiCfZoE && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=i69_gWvFH3A && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=P0NWROAgL94 && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=T2QZpy07j4s && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=4cEKAYnxbrk && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=vhbVKJbccz4 && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=1nd1T44tBkk && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=zIQr6P100dw && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=2HtToGzNo24 && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=V98I19bLr-o && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=qUundAa9j4M && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=u8sn9C3sID0 && \
+yt-dlp -P "~/Music" -x --audio-format mp3 https://www.youtube.com/watch?v=mA6XdqRUOGw
